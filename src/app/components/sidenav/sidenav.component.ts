ce55a047c1fd52536d9f97250e3ad2fdcb897572
morihara-y <yutaka@morihara.tech>
2024-10-24 14:19:29 +0000

    refs #1 create sidenav component

---
+++ src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,69 @@
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { SidemenuComponent } from '../sidemenu/sidemenu.component';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { TEXT, Text } from '../../../resources/texts/text';
+import { SidemenuItemModel, SidemenuPersonModel } from '../sidemenu/sidemenu-model';
+
+@Component({
+  selector: 'app-sidenav',
+  standalone: true,
+  imports: [
+    SidemenuComponent,
+    MatSidenavModule
+  ],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.scss'
+})
+export class SidenavComponent implements OnChanges {
+  @ViewChild('sidenav') sidenav?: MatSidenav;
+
+  @Input() toggle: boolean = false;
+  @Output() toggleChange: EventEmitter<boolean> = new EventEmitter();
+
+  text?: Text;
+  sidemenuItems?: Array<SidemenuItemModel>;
+  sidemenuPerson?: SidemenuPersonModel;
+
+  ngOnChanges(): void {
+    if (!this.sidenav) {
+      return;
+    }
+    if (this.toggle) {
+      this.sidenav.open();
+    } else {
+      this.sidenav.close();
+    }
+  }
+
+  constructor(
+    // @Inject(ENVIRONMENT) env: Environment
+  ) {
+    this.init();
+  }
+
+  onClickMenu(): void {
+    setTimeout(() => {
+      if (!this.toggle) {
+        this.toggleChange.emit(false);
+        this.sidenav?.close();
+      }
+    }, 100);
+  }
+
+  private init(): void {
+    // TODO locale
+    TEXT('ja').subscribe(res => {
+      this.text = res;
+      this.setSidemenu();
+    });
+  }
+
+  private setSidemenu(): void {
+    if (!this.text) {
+      return;
+    }
+    this.sidemenuItems = [
+      { icon: 'data_object', label: 'test', routerLink: '/' }
+    ];
+  }
+}
